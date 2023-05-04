401fe964137d52a33b35cc201eda805589739932
18-RAJAT <Rajatjoshi7535@gmail.com>
2023-05-04 14:51:41 +0530

    Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <h1>Portfolio</h1>
+      <Footer />
     </div>
   );
 }
